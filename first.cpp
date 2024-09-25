85db7494955d6591903ed0766ca8c80ef21b3df1
anchal chaturvedi <anchalchaturvedi08@gmail.com>
2024-09-25 17:59:28 +0530

    first

---
+++ first.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello! there" << endl;
+    // cout << 3 + 2 << endl;
+
+    // int a = 10;
+    // cout << "a";
+
+    // char c = 'a';
+    // cout << c;
+
+    float f = 2.3;
+    cout << f;
+
+    double d = 2.34;
+    cout << d;
+
+    bool b = true;
+    cout << b;
+}
