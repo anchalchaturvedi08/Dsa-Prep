78fd07412b359db750fc352c458cbbccd58f3dc7
anchal chaturvedi <anchalchaturvedi08@gmail.com>
2024-10-02 19:03:21 +0530

    first

--- Day_04/patternpart3.cpp
+++ Day_04/patternpart3.cpp
@@ -67,18 +67,124 @@ int main() {
     //     cout<<endl;
     // }
 
-    int n, row, col;
+    // int n, row, col;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+    // //space print
+    // for(row=1; row<=n; row=row+1){
+    //     for(col=1; col<=n-row; col=col+1){
+    //         cout<<"  ";
+    //     }
+    //     // print number
+    //     for(col=row; col>=1; col=col-1){
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int n, row, col;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+    // for(row=1; row<=n; row=row+1){
+    //     // space print
+    //     for(col=1; col<=n-row; col=col+1){
+    //         cout<<"  ";
+    //     }
+    //     // star print
+    //     for(col=1; col<=2*row-1; col=col+1){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int n, row, col;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+    // for(row=1; row<=n; row=row+1){
+    //     for(col=1; col<=n-row; col=col+1){
+    //         cout<<"  ";
+    //     }
+    //     for(col=1; col<=row; col=col+1)
+    //         cout<<col<<" ";
+    //     for(col=row-1; col>=1; col=col-1)
+    //         cout<<col<<" ";
+    //     cout<<endl;
+    // }
+
+    
+    // int n, row, col;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+    // for(row=n; row>=1; row=row-1){
+    //     // space print
+    //     for(col=1; col<=n-row; col=col+1){
+    //         cout<<"  ";
+    //     }
+    //     // star print
+    //     for(col=1; col<=2*row-1; col=col+1){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+//     int row, col, n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     // first half part
+//     for(row=n; row>=1; row=row-1){
+//         // print *
+//         for(col=1; col<=row; col=col+1)
+//             cout<<"* ";
+//         // print space
+//         for(col=1; col<=2*n-2*row; col=col+1)
+//             cout<<"  ";
+//         // print *
+//         for(col=1; col<=row; col=col+1)
+//             cout<<"* ";
+//         cout<<endl;
+//     }
+//     // second half part
+//     for(row=1; row<=n; row=row+1){
+//         // print *
+//         for(col=1; col<=row; col=col+1)
+//             cout<<"* ";
+//         // print space
+//         for(col=1; col<=2*n-2*row; col=col+1)
+//             cout<<"  ";
+//         // print *
+//         for(col=1; col<=row; col=col+1)
+//             cout<<"* ";
+//         cout<<endl;
+//     }
+
+    int row, col, n;
     cout<<"Enter the number: ";
     cin>>n;
-    //space print
+    // first half part
     for(row=1; row<=n; row=row+1){
-        for(col=1; col<=n-row; col=col+1){
+        // print *
+        for(col=1;col<=row;col=col+1)
+            cout<<"* ";
+        // space print
+        for(col=1;col<=2*n-2*row;col=col+1)
             cout<<"  ";
-        }
-        // print number
-        for(col=row; col>=1; col=col-1){
-            cout<<col<<" ";
-        }
+        // print *
+        for(col=1;col<=row;col=col+1)
+            cout<<"* ";
         cout<<endl;
     }
+    // second half part
+    for(row=n-1; row>=1; row=row-1){
+        // print *
+        for(col=1;col<=row;col=col+1)
+            cout<<"* ";
+        // space print
+        for(col=1;col<=2*n-2*row;col=col+1)
+            cout<<"  ";
+        // print *
+        for(col=1;col<=row;col=col+1)
+            cout<<"* ";
+        cout<<endl;
+    }
+
 }
